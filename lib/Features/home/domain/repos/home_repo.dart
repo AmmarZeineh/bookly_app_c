ba487a58509c36eab48e6f8ed958cbad4f651d77
AmmarZeineh <ammarzeineh3@gmail.com>
2025-03-29 03:19:48 +0300

    Refactor HomeRepo to return Either<Failure, List<BookEntity>> for book fetching methods and add Failure class for error handling

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app_clean_arc/Features/home/domain/entites/book_entity.dart';
+import 'package:bookly_app_clean_arc/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
