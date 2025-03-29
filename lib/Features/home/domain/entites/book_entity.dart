c5f101dc6f7621063b26c4255c4853410d121183
AmmarZeineh <ammarzeineh3@gmail.com>
2025-03-29 03:12:18 +0300

    Add BookEntity class to represent book details

---
+++ lib/Features/home/domain/entites/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String title;
+  final String author;
+  final num price;
+  final String imageUrl;
+  final num rating;
+
+  BookEntity({
+    required this.title,
+    required this.author,
+    required this.price,
+    required this.imageUrl,
+    required this.rating,
+  });
+}
